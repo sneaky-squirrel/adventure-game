e3d55c47a2458a7ed616d0b465fdf022d4f98b73
Michael Lee <michael.lee03@email.saintleo.edu>
2021-10-26 15:57:41 -0400

    Worked on healing

--- src/battle.c
+++ src/battle.c
@@ -8,23 +8,34 @@ battle: (Attack, Heal, Run Away) )
 
 int main (void) {
 
-int mainCharacter;
-char choice, attack, heal, runAway;
-//right down the 
+int attack, heal, currentHealth;
+char choice;
+//right down the pages 
+
+heal = 10; //This is your base health. 
+currentHealth = heal;
+
 printf("--------------------------------------------------------------\n\n");
 printf("----------------A wild monster appeared!----------------------\n\n");
+printf("This is your Current Health: %d\n\n", currentHealth);
 printf("Please enter an option:\n");
-printf("Press 'a' to attack.\n");
-printf("Press 'h' to heal.\n");
-printf("Press 'r' to run away from the fight.\n");
+printf("1) Press 'a' to attack.\n");
+printf("2) Press 'h' to heal.\n");
+printf("3) Press 'r' to run away from the fight.\n");
 printf("--------------------------------------------------------------\n");
 
-scanf(" %c", &choice);
+scanf(" %c", &choice); //This is input for making your choice
 
-switch (choice) {
+switch (choice) { //used a switch statement from Chapter 3.11, Switch Statements of ZyBooks
     case 'a' : printf("You attacked the monster!\n");
+    //need hitpoints and attack
     break;
-    case 'h' : printf("You healed yourself!\n");
+    case 'h' : printf("You healed yourself!\n\n");
+        //heal = 10;
+        currentHealth = heal + 15;
+        printf("This is your Current Health: %d\n", currentHealth);
+        heal++;
+        //printf("This is your Current Health: %d\n", currentHealth);
     break;
     case 'r' : printf("You ran away from the monster!\n");
     break;
