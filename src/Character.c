36271a9289f1c5a5bc7f8f90fbea2049f24418aa
moqasim <mqasim@bu.edu>
2021-10-27 11:34:59 -0400

    updated character

--- src/Character.c
+++ src/Character.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
-int main() {
-    printf("Test \n");
+int main(void) {
+    // character with HP
+    //character that can deal damage
+    // different class of characters 
+
+    
+     int characterSelection;
+    scanf(" %d", &characterSelection);
+    printf("Choose your character: %d\n", characterSelection);
+    printf("1. Wizard\n");
+    printf("2. Soldier\n");
+    printf("3. Engineer\n"); 
+    
+
+
+
+
+
     return 0;
 }
+
+
+
+    
