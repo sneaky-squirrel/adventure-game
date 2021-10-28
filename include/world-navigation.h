91d4e6ca4a62414ad30082651355dd7cf6b59cc2
Diego Gonzalez-Rodriguez <diego.gonzalez.rodriguez.games@gmail.com>
2021-10-28 10:35:33 -0700

    Added comment on how to expand the code, given the project had more time to be completed.

--- include/world-navigation.h
+++ include/world-navigation.h
@@ -1,6 +1,18 @@
 #ifndef WORLD_NAVIGATION_H
 #define WORLD_NAVIGATION_H
 
+/*
+ * If time permits, this file can be extended to include additional features such as:
+ * 	+ Multiple Levels instead of a single level.
+ * 	+ Variable Level Size
+ * 		Current implementation only allows a fixed level size of 1024x1024
+ * 	+ Restricted Movement
+ * 		e.g. Restrict movement in certain directions such as North or West when inside certain cells.
+ * 	+ List of Interactable Objects inside of a Location cell.
+ * 		e.g. NPC character with interactive dialogue.
+ * 		e.g. Monster to fight against.
+ * 		e.g. Items to pick up.
+ */
 namespace world
 {
 	typedef enum
