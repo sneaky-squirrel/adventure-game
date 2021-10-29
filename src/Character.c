2e1049252e08a3eb3e7ad04a2537d839a950bcbb
moqasim <mqasim@bu.edu>
2021-10-29 10:35:32 -0400

    update

--- src/Character.c
+++ src/Character.c
@@ -1,27 +1,22 @@
 #include <stdio.h>
-int health = 10;
+#include "../include/Character.h"
+int health = 10; // every character starts off with a max health of 10 HP.
 char weapons[128];
-//weapons each character should have is 
-//Wizard = Magic wand, staff, 
 
 
 void Wizard(){
-
 printf("You have selected the Wizard.\n");
-printf("Your health is %dHP\n", health);
-
+printf("Your health is %dHP\n\n", health);
 }
 
 void Elf(){
-
 printf("You have selected the Elf.\n");
-printf("Your health is %dHP\n", health);
+printf("Your health is %dHP\n\n", health);
 }
 
 void Knight(){
-
 printf("You have selected the Knight.\n");
-printf("Your health is %dHP\n", health);
+printf("Your health is %dHP\n\n", health);
 }
 
 
@@ -39,6 +34,7 @@ int main(void) {
     if (characterSelection == 'A' || characterSelection == 'a'){
         Wizard();
        
+       
     } 
     else if ( characterSelection == 'B' || characterSelection == 'b'){
          Elf ();
@@ -51,6 +47,10 @@ int main(void) {
     else {
         printf("Please only enter A, B or C\n");
     }
+     printf("----------------Choose your weapon of choice! ----------------------\n\n");
+
+
+    
     
     return 0;
 }
